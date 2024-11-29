f04aacefee5a3f3ec30537094a1cadde0aac3bfc
amiao <xiaolv516@gmail.com>
2024-11-29 21:19:25 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,390 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <link rel="icon" type="image/webp" href="MAOZHUA.webp"/>
+    <title>阿喵科技:APP喵</title>
+    
+    <meta name="keywords" content="阿喵科技,阿喵,自媒体" />
+    <meta name="description" content="阿喵科技有限公司" />
+    
+        <style type='text/css'>
+        
+body {
+    font-family: '微软雅黑';
+    background-image: url(https://www.appmiu.com/gh/abcmiao/img@main/pic/20210802093431.png);
+}
+
+p {
+    color: black;
+}
+
+
+
+.tile {
+    display: inline-block;
+    width: 300px;
+    height: 200px;
+    margin: 1px;
+    padding: 0;
+    overflow: hidden;
+    /*background-color: blue;*/
+    color: white;
+    font-family: '微软雅黑';
+    font-size: 20px;
+    vertical-align: middle;
+    cursor: pointer !important;
+    border-radius:15px;
+    box-shadow: 8px 8px 8px #eee;
+    margin:0 auto;
+    
+}
+
+    .tile label {
+        width: 300px;
+        height: 200px;
+        display: block;
+    }
+
+    .tile .title {
+        display: inline-block;
+        height: 200px;
+        width:300px;
+        line-height: 330px;
+        vertical-align: middle;
+        text-align: center;
+    }
+
+    .tile .content {
+        position: relative;
+        height: 200px;
+        padding: 8px;
+        display: block;
+        word-wrap: break-word;
+        word-break: break-all;
+        font-family: '微软雅黑';
+        font-size: 15px;
+          text-decoration:none;
+    }
+
+    .tile:hover {
+        -moz-box-shadow: 2px 2px 5px #ddd;
+        -webkit-box-shadow: 2px 2px 5px #ddd;
+        box-shadow: 2px 2px 5px #ddd;
+          text-decoration:none;
+    }
+
+        .tile:hover .content {
+            margin-top: -50px;
+            text-decoration:none;
+           
+        }
+
+    .tile input[type='checkbox'], .tile input[type='radio'] {
+        width: 40px;
+        height: 40px;
+        margin: 0;
+        padding: 0;
+        float: right;
+        position: relative;
+        outline: none !important;
+        border: 0 !important;
+        top: 0;
+        right: 0;
+        display: none;
+    }
+
+    .tile .symbol {
+        display: inline-block !important;
+        width: 40px;
+        height: 40px;
+        position: relative;
+        top: 2px;
+        right: 2px;
+        float: right;
+        margin-bottom: -40px;
+        z-index: 10000;
+    }
+
+   
+/*颜色*/
+.tile-mmq {
+    background-image: url(./喵喵签.webp);
+    color: black;
+    overflow:hidden;
+}
+
+    .tile-mmq .content {
+        background-color: deeppink;
+        /*background-image: url(./喵喵签.webp);*/
+        color: blue;
+    }
+
+.tile-ipa {
+    background-image: url(./ipa.png);
+    color: black;
+}
+
+    .tile-ipa .content {
+        background-color: deeppink;
+        color: green;
+    }
+
+
+
+.tile-app {
+  background-image: url(./APP喵.webp);  
+    color: black;
+}
+
+    .tile-app .content {
+        background-color: deeppink;
+        color: yellow;
+    }
+
+
+
+.tile-more {
+    background-image: url(./more.png);  
+    color: black;
+}
+
+    .tile-more .content {
+        background-color: deeppink;
+        color: yellow;
+    }
+
+/*第二行*/
+
+
+
+
+.tile-weixin {
+    background-image: url(./gzh.png);  
+    color: black;
+}
+
+    .tile-weixin .content {
+        background-color: deeppink;
+        color: yellow;
+    }
+
+.tile-bili {
+    background-image: url(./bili.png);  
+    color: black;
+}
+
+    .tile-bili .content {
+        background-color: deeppink;
+        color: yellow;
+    }
+
+.tile-tiktok {
+    background-image: url(./douyin.png);  
+    color: black;
+}
+
+    .tile-tiktok .content {
+        background-color: deeppink;
+        color: yellow;
+    }
+
+.tile-youtube {
+    background-image: url(./QQ.png);  
+    color: black;
+}
+
+    .tile-youtube .content {
+        background-color: deeppink;
+        color: yellow;
+    }
+
+
+</style>
+</head>
+
+<body>
+    
+ 
+    
+    
+<center>    
+    
+    <p>
+      <span style="font-size:49px; color:red;"> 阿喵科技 </span>
+      <span style="font-style:italic; font-size:19px; color:green;">corporation</span>
+    </p>
+    
+    
+<center>
+    <div class="All">
+        <div class="AllText">
+            <a class="Subject"></a>
+            <center><p class="Info">希望大家多多支持阿喵。你的支持是我坚持更新分享的动力，加油各位&nbsp;&nbsp;</p></center>
+        </div>
+    </div>
+</center>       
+    
+    
+    
+    
+       
+    
+    
+    
+    <p><H1>网站（iOS,Andriod,macOS,Windows）</H1></p>
+    
+    
+        <div class="tile tile-app">
+        <label>
+            <input type="checkbox" />
+            <span class="symbol">
+            </span>
+            <a href="https://www.appmiu.com" >
+            <span class="title">
+                APP喵:网站
+            </span>
+            </a>
+            <span class="content"> <a class="tile .content" id="install" data-clipboard-text="" href="https://www.appmiu.com" target="_blank" >点击跳转</a></span>
+        </label>
+    </div>
+    
+    
+    <div class="tile tile-mmq">
+            
+        <label>
+            <input type="checkbox" />
+            <span class="symbol">
+               
+            </span><!--这个地方“<span class="symbol"></span>”刚才被编辑器直接过滤掉，也过于“智能”了吧，不是所有的空标签就真的是没有用的……好吧，这样“<span class="symbol"> </span>”，终于把效果效果保住了——这可是关系到钩钩的显示-->
+            <a href="https://sim.amiao.co">
+                <span class="title">
+                阿喵手机卡 
+            </span>
+            </a>
+            <span class="content">
+                <!--<a href="https://amiao.app" target="_blank" >点击跳转</a>-->
+                <a class="tile .content" id="install" data-clipboard-text="" href="https://sim.amiao.co" target="_blank" >点击跳转</a>
+                </span>
+        </label>
+    </div>
+    
+    <div class="tile tile-ipa">
+        <label>
+            <input type="checkbox" />
+            <span class="symbol">
+            </span>
+            <a href="https://www.appmz.com" target="_blank" >
+            <span class="title">
+                应用周刊：每周APP推荐
+            </span>
+            </a>
+            <span class="content"> <a class="tile .content" id="install" data-clipboard-text="" href="https://www.appmz.com" target="_blank" >点击跳转</a></span>
+        </label>
+    </div>
+    
+    
+    
+    
+
+    
+    
+    
+    
+    <div class="tile tile-more">
+        <label>
+            <input type="checkbox" />
+            <span class="symbol">
+            </span>
+            <span class="title">
+                更多资源
+            </span>
+            <span class="content">敬请期待</span>
+        </label>
+    </div>
+
+
+
+
+
+
+
+    <p><H1>自媒体（短视频，公众号等 [APP喵:阿喵软件]）</H1></p>
+   
+    <div class="tile tile-weixin">
+        <label>
+            <input type="checkbox" />
+            <span class="symbol">
+            </span>
+            <span class="title">
+                公众号
+            </span>
+            <span class="content">APP喵：扫码关注</span>
+        </label>
+    </div>
+    <div class="tile tile-bili">
+        <label>
+            <input type="checkbox" />
+            <span class="symbol">
+            </span>
+            <a href="https://space.bilibili.com/6623126" target="_blank" >
+            <span class="title">
+                哔哩哔哩
+            </span>
+            </a>
+            <span class="content"> <a class="tile .content" id="install" data-clipboard-text="" href="https://space.bilibili.com/6623126" target="_blank" >戳我，一键三连</a></span>
+        </label>
+    </div>    
+    <div class="tile tile-tiktok">
+        <label>
+            <input type="radio" name="tile_radio" />
+            <span class="symbol">
+            </span>
+            <span class="title">
+                抖音
+            </span>
+            <span class="content">抖音号：appmiu</span>
+        </label>
+    </div>
+     <div class="tile tile-youtube">
+        <label>
+            <input type="radio" name="tile_radio" />
+            <span class="symbol">
+
+            </span>
+            <a href="https://www.youtube.com/channel/UCRY_sNzEV3Azzx9EzolsVog/featured">
+            <span class="title">
+                QQ群
+            </span>
+            </a>
+               <span class="content"> <a class="tile .content" id="install" data-clipboard-text="" href="https://jq.qq.com/?_wv=1027&k=8SAZjfWN" target="_blank" >点击链接加入群聊【APP喵】</a></span>
+        </label>
+    </div>
+    
+    
+    
+    
+    
+</center>     
+
+<br>
+<br>
+<center>    
+    
+    <p>
+    
+    </p>
+    
+    
+    		 	<div style="width:300px;margin:0 auto; padding:20px 0;">
+    		 		<a target="_blank" href="https://beian.miit.gov.cn/" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;"> 备案:京ICP备2022017863号-1</p></a>      
+		 		<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11011402012752" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/备案图标.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">京公网安备 11011402012752号</p></a>
+		 		
+		 	</div>
+		 
+    
+</center>       
+    
+</body>
+</html>
